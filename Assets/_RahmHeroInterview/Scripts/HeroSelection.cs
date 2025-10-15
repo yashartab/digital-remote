e07c021dac047e1054e8a1362008e40758a5d1f8
lpeuker-rahm <lea.peuker@rahm.de>
2025-10-15 11:26:22 +0200

    Add namespaces to all scene related scripts.

--- Assets/_RahmHeroInterview/Scripts/HeroSelection.cs
+++ Assets/_RahmHeroInterview/Scripts/HeroSelection.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class HeroSelection : MonoBehaviour
+namespace RahmHeroInterview
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class HeroSelection : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
--- Assets/_RahmHeroInterview/Scripts/TopicSelection.cs
+++ Assets/_RahmHeroInterview/Scripts/TopicSelection.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class TopicSelection : MonoBehaviour
+namespace RahmHeroInterview
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class TopicSelection : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
