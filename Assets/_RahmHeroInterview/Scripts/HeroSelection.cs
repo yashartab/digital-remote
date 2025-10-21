9dc03a1ac3d0c30e4a3b42457a520e4b5b03b88b
lpeuker-rahm <lea.peuker@rahm.de>
2025-10-21 07:38:09 +0200

    Modify hero card prefab and implement automatic hero card generation based on the hero data send from the mirror.

--- Assets/_RahmHeroInterview/Scripts/HeroSelection.cs
+++ Assets/_RahmHeroInterview/Scripts/HeroSelection.cs
@@ -1,29 +1,55 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RahmHeroInterview
 {
     public class HeroSelection : MonoBehaviour
     {
-        [SerializeField] private MsgHandler msgHandler;
-            
-        // private void Awake()
-        // {
-        //     msgHandler.OnInitHeroSelection();
-        // }
+        // Parent transform of hero cards
+        [SerializeField] private Transform heroCardsParent;
+        // Prefab for hero card
+        [SerializeField] private GameObject heroCardPrefab;
+        
+        // List of all hero cards
+        private List<HeroCard> heroCards = new List<HeroCard>();
+        
         
         void Update()
         {
-
+            // TODO: Automatic hero rotation
         }
 
         public void InitHeroSelection(List<RahmHeroData> heroData)
         {
+            // Generate the hero cards
             foreach (RahmHeroData hero in heroData)
             {
-                Debug.Log(hero.Id);
+                // Generate a new button for the hero
+                GameObject newButton = Instantiate(heroCardPrefab, heroCardsParent);
+                HeroCard heroCard = newButton.GetComponent<HeroCard>();
+                heroCard.SetHeroSelection(this);
+
+                // Add hero card to list
+                heroCards.Add(heroCard);
+                
+                // Set hero card content
+                heroCard.SetHeroID(hero.Id);
+                heroCard.SetNameText(hero.Name);
+                heroCard.SetKeywordText(hero.Keyword);
+                heroCard.SetDescriptionText(hero.Description);
+
+                // Add onClick event to the button which selects the hero
+                heroCard.GetComponent<Button>().onClick.AddListener(() => { this.OnSelectHero(hero); });
+
             }
+        }
+        
+        public void OnSelectHero(RahmHeroData hero)
+        {
+            // TODO
+            // interviewController.SelectHero(hero);
         }
     }
 }
